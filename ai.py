b35c6f604e98126e80202505f8cb86531c7e75e9
Lucas0418 <jslu0418@gmail.com>
2016-02-06 23:22:10 +0800

    Update ai.py, add group inventory

--- ai.py
+++ ai.py
@@ -13,8 +13,12 @@ dbname = 'djangodev'
 charset = 'utf8'
 
 # Defined the sqls would be used.
-allhostsql = 'select ansible_alias from inventory_host'
-findhostvarsql = 'select a.var_name,a.var_value from inventory_hostvar a where a.host_id = (select id from inventory_host b where b.ansible_alias = %s)'
+allhostsql = 'select id,ansible_alias from inventory_host'
+findhostvarsql = 'select a.var_name,a.var_value from inventory_hostvar a where a.host_id = %s'
+allgroupsql = 'select id,ansible_group from inventory_group'
+findgroupvarsql = 'select a.var_name,a.var_value from inventory_groupvar a where a.group_id  = %s'
+findgrouphostssql = 'select a.host_id from inventory_group_ansible_hosts a where a.group_id = %s'
+findgroupchildrensql = 'select a.to_group_id from inventory_group_ansible_children a where a.from_group_id = %s'
 
 
 def findHost(alias):
@@ -25,10 +29,10 @@ def findHost(alias):
     dbconn = MySQLdb.Connect(host=host, user=user, passwd=password, db=dbname, charset=charset)
     cursor = dbconn.cursor()
     cursor.execute(findhostvarsql, [alias])
-    data = cursor.fetchall()
-    data = dict(data)
+    vardata = cursor.fetchall()
+    vardata = dict(vardata)
     dbconn.close()
-    print json.dumps(data, sort_keys=True, indent=2)
+    print json.dumps(data, indent=2)
 
 
 def findAll():
@@ -40,21 +44,57 @@ def findAll():
     cursor = dbconn.cursor()
     cursor.execute(allhostsql)
     hostdata = cursor.fetchall()
-    hostdata = [x[0] for x in hostdata]
-    resultdata = {}
-    allhostvardata = {}
-    allhostdata = {'hosts': hostdata}
-    # the 'all' element.
-    resultdata['all'] = allhostdata
-    for alias in hostdata:
-        cursor.execute(findhostvarsql, [alias])
-        data = cursor.fetchall()
-        data = dict(data)
-        allhostvardata[alias] = data
+    hostids = [x[0] for x in hostdata]
+    hosts = [x[1] for x in hostdata]
+    hostdata = dict(hostdata)
+    result = {}
+    allhostvar = {}
+    allhostdata = {'hosts': hosts}
+    # the 'all' element contains every host alias.
+    result['all'] = allhostdata
+    for i in range(0, len(hostids)):
+        cursor.execute(findhostvarsql, [hostids[i]])
+        vardata = cursor.fetchall()
+        vardata = dict(vardata)
+        allhostvar[hosts[i]] = vardata
     # for ansible 1.3 or higher, add the top element '_meta' for all hostvars.
-    resultdata['_meta'] = {'hostvars': allhostvardata}
+    result['_meta'] = {'hostvars': allhostvar}
+    # group elements
+    # prepare datas
+    cursor.execute(allgroupsql)
+    groupdata = cursor.fetchall()
+    groupids = [x[0] for x in groupdata]
+    groups = [x[1] for x in groupdata]
+    groupdata = dict(groupdata)
+    for i in range(0, len(groupids)):
+        # vars in group
+        cursor.execute(findgroupvarsql, [groupids[i]])
+        vardata = cursor.fetchall()
+        vars = dict(vardata)
+        # hosts in group
+        cursor.execute(findgrouphostssql, [groupids[i]])
+        hostsiddata = cursor.fetchall()
+        hosts = [hostdata[x[0]] for x in hostsiddata]
+        # children in group
+        cursor.execute(findgroupchildrensql, [groupids[i]])
+        groupchildreniddata = cursor.fetchall()
+        groupchildren = [groupdata[x[0]] for x in groupchildreniddata]
+        if len(vars) == 0 and len(groupchildren) == 0:
+            if len(hosts) > 0:
+                result[groups[i]] = hosts
+            else:
+                continue
+        else:
+            thisgroupdata = {}
+            if len(hosts) > 0:
+                thisgroupdata['hosts'] = hosts
+            if len(vars) > 0:
+                thisgroupdata['vars'] = vars
+            if len(groupchildren) > 0:
+                thisgroupdata['children'] = groupchildren
+            result[groups[i]] = thisgroupdata
     dbconn.close()
-    print json.dumps(resultdata, sort_keys=True, indent=2)
+    print json.dumps(result, indent=2)
 
 args = sys.argv
 if len(args) > 2 and args[1] == '--host':
